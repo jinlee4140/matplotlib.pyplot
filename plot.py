7df603279855ab05f686dc93eb5d97f75f5c1e1d
jinlee4140 <jinlee4140@gmail.com>
2015-09-09 17:34:07 -0400

    local minima

--- plot.py
+++ plot.py
@@ -82,8 +82,10 @@ s = np.cos(2*np.pi*t)
 line, = plt.plot(t, s, lw=2)
 
 plt.annotate('local maxima', xy=(2, 1), xytext=(3, 1.5),
-            arrowprops=dict(facecolor='black', shrink=0.05),
+            arrowprops=dict(facecolor='black', shrink=0.05)
             )
-
+plt.annotate('local minima', xy=(2.5, -1), xytext=(3, -1.5),
+			arrowprops=dict(facecolor='black', shrink=0.05)
+			)
 plt.ylim(-2,2)
 plt.show()
